e7e6254e59cf4a909cfb00769a61d3a32b478211
yehia-khalil <yehiakhalilykk@gmail.com>
2023-10-11 23:29:59 +0300

    init

---
+++ app/Http/Controllers/TransactionPaymentController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreTransactionPaymentRequest;
+use App\Http\Requests\UpdateTransactionPaymentRequest;
+use App\Models\TransactionPayment;
+
+class TransactionPaymentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreTransactionPaymentRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(TransactionPayment $transactionPayment)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(TransactionPayment $transactionPayment)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateTransactionPaymentRequest $request, TransactionPayment $transactionPayment)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(TransactionPayment $transactionPayment)
+    {
+        //
+    }
+}
---
+++ database/migrations/2023_10_11_182006_create_transactions_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('transaction_category_id')->constrained();
+            $table->integer('amount');
+            $table->foreignId('payer')->references('id')->on('users');
+            $table->date('due_date');
+            $table->integer('vat');
+            $table->boolean('is_vat_inclusive');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
